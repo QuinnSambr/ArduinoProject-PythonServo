6ef8a4077fa3ba01870695cb902effd29e1e88f5
QuinnyBoy-TT <59735361+QuinnyBoy-TT@users.noreply.github.com>
2020-07-09 16:47:38 -0400

    Update ArduinoCommands_Send.py

--- ArduinoCommands_Send.py
+++ ArduinoCommands_Send.py
@@ -1,30 +1,17 @@
-# import serial
-# import time
-
-# ser=serial.Serial('COM3',baudrate=9600, timeout=1)
-# time.sleep(3)
-# def getVal():
-#     ser.write(b'g') #B is for writing byte
-#     ad = ser.readline().decode('ascii')
-#     return ad
-
-# while(1):
-#     userInput=input('Get Data Point ?\n')
-
-#     if userInput=='y':
-#         print(getVal())
-
-
-
+#Written by Quinn Sambrano 
+#Version 2.1
+#Arduino integration with Python
 
 import pyautogui, sys
 from tkinter import * 
+
+#Function tracks X,Y cords in real time within the generated window
 def cords(events):
     x, y = pyautogui.position()
     positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
     label['text']=positionStr
     
-
+#Main function wich generates a window of size 600x600
 w=Tk()
 canvas=Canvas(width=600,height=600)
 label=Label(bd=4 ,bg="white",fg="black")
@@ -32,3 +19,4 @@ canvas.bind("<Motion>",cords)
 canvas.grid(row=0,column=0)
 label.grid(row=1,column=0)
 w.mainloop()
+
